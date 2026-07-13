1bdd697727af88adf30de59e882e0e17bc4515ab
Unjuno <unjuno.org@unjuno.org>
2026-07-14 04:43:41 +0900

    Export v2 training and recovery types

--- src/opfusion/training/__init__.py
+++ src/opfusion/training/__init__.py
@@ -1,11 +1,20 @@
-from .config import OptimizerConfig, RunConfig, load_run_config
-from .data import EXPERIMENT_OPERATORS, SyntheticDataConfig, SyntheticTraceFactory
+from .config import OptimizerConfig, RecoveryConfig, RunConfig, load_run_config
+from .data import (
+    EXPERIMENT_OPERATORS,
+    EncodedTrainingExample,
+    SyntheticDataConfig,
+    SyntheticTraceFactory,
+    TrainingExample,
+)
 
 __all__ = [
     "OptimizerConfig",
+    "RecoveryConfig",
     "RunConfig",
     "load_run_config",
     "EXPERIMENT_OPERATORS",
     "SyntheticDataConfig",
     "SyntheticTraceFactory",
+    "TrainingExample",
+    "EncodedTrainingExample",
 ]
